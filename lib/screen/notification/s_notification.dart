d486a2579e70cdb8a1b7c84242962b1afb429edd
hani <hanisoft2022@gmail.com>
2025-04-07 20:56:29 +0900

    start notification configuration

--- lib/screen/notification/s_notification.dart
+++ lib/screen/notification/s_notification.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+class NotifiationScreen extends HookConsumerWidget {
+  const NotifiationScreen({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
